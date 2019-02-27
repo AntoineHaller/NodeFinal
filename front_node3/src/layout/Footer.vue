092bd5f38b93fb0c200836fd92124ba83c511cef
EmmanuelEskander07 <34535442+EmmanuelEskander07@users.noreply.github.com>
2019-02-27 19:16:04 +0100

    Add files via upload

---
+++ front_node3/src/layout/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="footer">
+        <span>ICI C'EST LE FOOTER</span>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+    #footer {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-weight: bolder;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: rebeccapurple;
+        margin-top: 20px;
+    }
+</style>
