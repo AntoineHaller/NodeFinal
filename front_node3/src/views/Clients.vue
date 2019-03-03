2561daec708e2e5be19999dcee1b2d6d23843775
AntoineHaller <34535243+AntoineHaller@users.noreply.github.com>
2019-03-03 16:16:44 +0100

    Add files via upload

--- front_node3/src/views/Clients.vue
+++ front_node3/src/views/Clients.vue
@@ -25,6 +25,48 @@
     </div>
 </template>
 
+<style>
+    table{
+        margin: auto;
+    }
+
+    td,th{
+        width: 10%;
+        height: auto;
+        padding-bottom: 2%;
+    }
+
+    table a{
+        background-color: #34495e;
+        color: #42b983;
+        border: none;
+        font-size: 15px;
+        border-radius: 5px;
+        text-decoration: none;
+        padding: 3%;
+    }
+
+    table a:hover{
+        background-color: #42b983;
+        color: #34495e;
+    }
+
+    button{
+        background-color: #34495e;
+        color: #42b983;
+        border: none;
+        font-size: 15px;
+        border-radius: 5px;
+        padding: 3%;
+    }
+
+    button:hover{
+        background-color: #c0392b;
+        color: #fff;
+    }
+
+</style>
+
 <script>
     import ClientsService from '@/services/ClientsService.vue'
 
